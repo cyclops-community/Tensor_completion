c1cf6a8a8d3f92337f0cd39681abbb2b86cbe2bb
Edgar Solomonik <solomon2@illinois.edu>
2019-10-07 13:47:00 -0500

    remove auxiliary/old/unused code

--- als.py
+++ als.py
@@ -1,55 +1,14 @@
 import ctf,time,random
 import numpy as np
 import numpy.linalg as la
-#import matplotlib
-#matplotlib.use('Agg')
-#import matplotlib.pyplot as plt
 from ctf import random as crandom
 glob_comm = ctf.comm()
-#from scipy.sparse.linalg import lsqr as lsqr
 import sys
-#sys.path.append('../data/function_tensor')
-#from function_tensor import *
 from math import sqrt
 
 status_prints = False
 CG_thresh = 1.e-4
 sparse_format = True
-
-
-def function_tensor(I, J, K, sparsity):
-    # N = 5
-    # n = 51
-    # L = 100
-    # nsample = 10*N*n*L #10nNL = 255000
-
-    T = ctf.tensor((I, J, K), sp=True)
-    T2 = ctf.tensor((I, J, K), sp=True)
-
-    T.fill_sp_random(1, 1, sparsity)
-    # T = ctf.exp(-1 * ctf.power(ctf.power(T,2),0.5))  # x = exp(-sqrt(x^2))
-
-    sizes = [I, J, K]
-    index = ["i", "j", "k"]
-
-    for i in range(3):
-        n = sizes[i]
-        v = np.linspace(-1, 1, n)
-        # v = np.arange(1,n+1)
-        v = ctf.astensor(v ** 2)
-
-        v2 = ctf.tensor(n, sp=True)
-        v2 = v
-        T2.i("ijk") << T.i("ijk") * v2.i(index[i])
-
-    # T2 = ctf.power(T2, 0.5)
-    [inds, data] = T2.read_local_nnz()
-    data[:] **= .5
-    data[:] *= -1.
-    T2 = ctf.tensor(T2.shape,sp=True)
-    T2.write(inds,data)
-
-    return T2
 
 class implicit_ATA:
     def __init__(self, f1, f2, omega, string):
@@ -136,15 +95,6 @@ def CG(A,b,x0,r,regParam,I,is_implicit=False):
 
     t_batch_cg.stop()
     return xk
-
-
-def solve_SVD(A,b,factor,r,regParam):
-    [U_,S_,VT_] = ctf.svd(A)
-    S_ = 1/(S_+regParam*ctf.ones(S_.shape))
-    factor.set_zero()
-    factor.i("r") << VT_.i("kr")*S_.i("k")*U_.i("tk")*b.i("t")
-
-    return factor
 
 def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
 
@@ -244,19 +194,7 @@ def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
 
         return W
 
-def solve(A,b,factor,r,regParam):
-    n = A.shape[0]
-    L = ctf.cholesky(A+regParam*ctf.eye(n))
-    factor = ctf.solve_tri(L,b.reshape((n,1)),True,True,False)
-    factor = ctf.solve_tri(L,factor,True,True,True).reshape((n,))
-
-    return factor
-
-
 def getALS_CG(T,U,V,W,regParam,omega,I,J,K,r,block_size,num_iter=100,err_thresh=.001,time_limit=600,use_implicit=True):
-
-
-
     if use_implicit == True:
         t_ALS_CG = ctf.timer_epoch("als_CG_implicit")
         if ctf.comm().rank() == 0:
@@ -332,100 +270,4 @@ def getALS_CG(T,U,V,W,regParam,omega,I,J,K,r,block_size,num_iter=100,err_thresh=
 
     if glob_comm.rank() == 0:
         print('ALS (implicit =',use_implicit,') time per sweep: {}'.format(duration/it))
-
-
-def getOmega_old(T,I,J,K):
-    if (T.sp==False):
-        omegactf = ((T > 0)*ctf.astensor(1.))
-    else:
-        omegactf = T / T
-
-    return omegactf
-
-def getOmega(T):
-    [inds, data] = T.read_local_nnz()
-    data[:] = 1.
-    Omega = ctf.tensor(T.shape,sp=T.sp)
-    Omega.write(inds,data)
-    return Omega
-
-
-def main():
-
-    I = 1000
-    J = 1000
-    K = 1000
-    r = 2
-    sparsity = .000001
-    regParam = .1
-    block_size = 1000
-    use_func = 0
-    num_iter = 1
-    err_thresh = .001
-    run_implicit = 1
-    run_explicit = 1
-
-    if len(sys.argv) >= 4:
-        I = int(sys.argv[1])
-        J = int(sys.argv[2])
-        K = int(sys.argv[3])
-    if len(sys.argv) >= 5:
-        sparsity = np.float64(sys.argv[4])
-    if len(sys.argv) >= 6:
-        r = int(sys.argv[5])
-    if len(sys.argv) >= 7:
-        block_size = int(sys.argv[6])
-    if len(sys.argv) >= 8:
-        use_func= int(sys.argv[7])
-    if len(sys.argv) >= 9:
-        num_iter = int(sys.argv[8])
-    if len(sys.argv) >= 10:
-        err_thresh = np.float64(sys.argv[9])
-    if len(sys.argv) >= 11:
-        regParam= np.float64(sys.argv[10])
-    if len(sys.argv) >= 12:
-        run_implicit= int(sys.argv[11])
-    if len(sys.argv) >= 13:
-        run_explicit= int(sys.argv[12])
-
-
-    if glob_comm.rank() == 0:
-        print("I is",I,"J is",J,"K is",K,"sparisty is",sparsity,"r is",r,"block_size is",block_size, "use_func is",use_func,"num_iter is",num_iter,"err_thresh is",err_thresh,"regParam is",regParam,"run_implicit",run_implicit,"run_explicit is",run_explicit)
-
-
-    # 3rd-order tensor
-    if use_func==1:
-        T_SVD = function_tensor(I,J,K,sparsity)
-    else:
-        T_SVD = ctf.tensor((I,J,K),sp=sparse_format)
-        T_SVD.fill_sp_random(0,1,sparsity)
-
-
-    #omega = updateOmega(T_SVD,I,J,K)
-    t0 = time.time()
-    omega = getOmega(T_SVD)
-    if glob_comm.rank() == 0:
-        print('getOmega takes {}'.format(time.time() - t0))
-
-    ctf.random.seed(42)
-    U_SVD = ctf.random.random((I,r))
-    V_SVD = ctf.random.random((J,r))
-    W_SVD = ctf.random.random((K,r))
-
-    U_CG = ctf.copy(U_SVD)
-    V_CG = ctf.copy(V_SVD)
-    W_CG = ctf.copy(W_SVD)
-    T_CG = ctf.copy(T_SVD)
-
-    if run_implicit == True:
-        getALS_CG(T_CG,U_CG,V_CG,W_CG,regParam,omega,I,J,K,r,block_size,num_iter,err_thresh,600,True)
-    if run_explicit == True:
-        getALS_CG(T_SVD,U_SVD,V_SVD,W_SVD,regParam,omega,I,J,K,r,block_size,num_iter,err_thresh,600,False)
-
-
-
-#main()
-if __name__ == '__main__':
-    main()
-
 
