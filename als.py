4feba59e0f6dd1364b8859eaea608460e0c6adbc
Edgar Solomonik <solomon2@illinois.edu>
2019-12-07 23:01:01 -0600

    add (so far nonworking) potential support for MTTKRP

--- als.py
+++ als.py
@@ -11,25 +11,40 @@ CG_thresh = 1.e-4
 sparse_format = True
 
 class implicit_ATA:
-    def __init__(self, f1, f2, omega, string):
+    def __init__(self, f1, f2, omega, string, use_MTTKRP):
         self.f1 = f1
         self.f2 = f2
         self.omega = omega
         self.string = string
+        self.use_MTTKRP = use_MTTKRP
 
     def mul(self, idx, sk):
-        if self.string=="U":
-            return  self.f1.i("J"+idx[1]) \
-                   *self.f2.i("K"+idx[1]) \
-                   *ctf.TTTP(self.omega, [sk, self.f1, self.f2]).i(idx[0]+"JK")
-        if self.string=="V":
-            return  self.f1.i("I"+idx[1]) \
-                   *self.f2.i("K"+idx[1]) \
-                   *ctf.TTTP(self.omega, [self.f1, sk, self.f2]).i("I"+idx[0]+"K")
-        if self.string=="W":
-            return  self.f1.i("I"+idx[1]) \
-                   *self.f2.i("J"+idx[1]) \
-                   *ctf.TTTP(self.omega, [self.f1, self.f2, sk]).i("IJ"+idx[0])
+        if self.use_MTTKRP:
+            if self.string=="U":
+                out = ctf.tensor([self.omega.shape[0], self.f1.shape[1]])
+                ctf.MTTKRP(ctf.TTTP(self.omega, [sk, self.f1, self.f2]),[out,self.f1,self.f2],0)
+            elif self.string=="V":
+                out = ctf.tensor([self.omega.shape[1], self.f1.shape[1]])
+                ctf.MTTKRP(ctf.TTTP(self.omega, [self.f1, sk, self.f2]),[self.f1,out,self.f2],1)
+            elif self.string=="W":
+                out = ctf.tensor([self.omega.shape[2], self.f1.shape[1]])
+                ctf.MTTKRP(ctf.TTTP(self.omega, [self.f1, self.f2, sk]),[self.f1,self.f2,out],2)
+            else:
+                return ValueError("Invalid string for MTTKRP_mul")
+            return out.i(idx)
+        else:
+            if self.string=="U":
+                return  self.f1.i("J"+idx[1]) \
+                       *self.f2.i("K"+idx[1]) \
+                       *ctf.TTTP(self.omega, [sk, self.f1, self.f2]).i(idx[0]+"JK")
+            if self.string=="V":
+                return  self.f1.i("I"+idx[1]) \
+                       *self.f2.i("K"+idx[1]) \
+                       *ctf.TTTP(self.omega, [self.f1, sk, self.f2]).i("I"+idx[0]+"K")
+            if self.string=="W":
+                return  self.f1.i("I"+idx[1]) \
+                       *self.f2.i("J"+idx[1]) \
+                       *ctf.TTTP(self.omega, [self.f1, self.f2, sk]).i("IJ"+idx[0])
 
 def CG(A,b,x0,r,regParam,I,is_implicit=False):
 
@@ -96,8 +111,7 @@ def CG(A,b,x0,r,regParam,I,is_implicit=False):
     t_batch_cg.stop()
     return xk
 
-def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
-
+def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit,use_MTTKRP):
     t_RHS = ctf.timer("ALS_imp_cg_RHS")
     t_cg_TTTP = ctf.timer("ALS_imp_cg_TTTP")
     t_o_slice = ctf.timer("ALS_imp_omega_slice")
@@ -110,28 +124,41 @@ def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
             I_end = min(I,I_start+block_size)
             bsize = I_end-I_start
             t_o_slice.start()
-            nomega = omega[I_start : I_end,:,:]
+            if num_blocks > 1:
+                nomega = omega[I_start : I_end,:,:]
+            else:
+                nomega = omega
             t_o_slice.stop()
             x0 = ctf.random.random((bsize,r))
             b = ctf.tensor((bsize,r))
             t_RHS.start()
-            b.i("ir") << V.i("Jr")*W.i("Kr")*T[I_start : I_end,:,:].i("iJK")  # RHS; ATb
+            if num_blocks == 1:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T, [b, V, W], 0)
+                else:
+                    b.i("ir") << V.i("Jr")*W.i("Kr")*T.i("iJK")
+            else:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T[I_start : I_end,:,:], [b, V, W], 0)
+                else:
+                    b.i("ir") << V.i("Jr")*W.i("Kr")*T[I_start : I_end,:,:].i("iJK")  # RHS; ATb
             t_RHS.stop()
-
             if use_implicit:
                 Ax0 = ctf.tensor((bsize,r))
                 t_cg_TTTP.start()
-                Ax0.i("ir") << V.i("Jr")*W.i("Kr")*ctf.TTTP(nomega, [x0,V,W]).i("iJK")
+                if use_MTTKRP:
+                    ctf.MTTKRP(ctf.TTTP(nomega, [x0,V,W]), [Ax0, V, W], 0)
+                else:
+                    Ax0.i("ir") << V.i("Jr")*W.i("Kr")*ctf.TTTP(nomega, [x0,V,W]).i("iJK")
                 t_cg_TTTP.stop()
                 Ax0 += regParam*x0
-                U[I_start : I_end,:] = CG(implicit_ATA(V,W,nomega,"U"),b,x0,r,regParam,bsize,True)
+                U[I_start : I_end,:] = CG(implicit_ATA(V,W,nomega,"U",use_MTTKRP),b,x0,r,regParam,bsize,True)
             else:
                 A = ctf.tensor((bsize,r,r))
                 t_form_EQs.start()
                 A.i("iuv") << V.i("Ju")*W.i("Ku") * nomega.i("iJK")*V.i("Jv")*W.i("Kv")
                 t_form_EQs.stop()
                 U[I_start : I_end,:] = CG(A,b,x0,r,regParam,bsize)
-
         return U
 
     if (string=="V"):
@@ -141,20 +168,35 @@ def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
             J_end = min(J,J_start+block_size)
             bsize = J_end-J_start
             t_o_slice.start()
-            nomega = omega[:,J_start : J_end,:]
+            if num_blocks > 1:
+                nomega = omega[:,J_start : J_end,:]
+            else:
+                nomega = omega
             t_o_slice.stop()
             x0 = ctf.random.random((bsize,r))
             b = ctf.tensor((bsize,r))
             t_RHS.start()
-            b.i("jr") << U.i("Ir")*W.i("Kr")*T[:,J_start : J_end,:].i("IjK")  # RHS; ATb
+            if num_blocks == 1:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T, [U, b, W], 1)
+                else:
+                    b.i("jr") << U.i("Ir")*W.i("Kr")*T.i("IjK")  # RHS; ATb
+            else:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T[:,J_start : J_end,:], [U, b, W], 1)
+                else:
+                    b.i("jr") << U.i("Ir")*W.i("Kr")*T[:,J_start : J_end,:].i("IjK")  # RHS; ATb
             t_RHS.stop()
             if use_implicit:
                 Ax0 = ctf.tensor((bsize,r))
                 t_cg_TTTP.start()
-                Ax0.i("jr") << U.i("Ir")*W.i("Kr")*ctf.TTTP(nomega, [U,x0,W]).i("IjK")
+                if use_MTTKRP:
+                    ctf.MTTKRP(ctf.TTTP(nomega, [U,x0,W]), [U, Ax0, W], 1)
+                else:
+                    Ax0.i("jr") << U.i("Ir")*W.i("Kr")*ctf.TTTP(nomega, [U,x0,W]).i("IjK")
                 t_cg_TTTP.stop()
                 Ax0 += regParam*x0
-                V[J_start : J_end,:] = CG(implicit_ATA(U,W,nomega,"V"),b,x0,r,regParam,bsize,True)
+                V[J_start : J_end,:] = CG(implicit_ATA(U,W,nomega,"V",use_MTTKRP),b,x0,r,regParam,bsize,True)
             else:
                 A = ctf.tensor((bsize,r,r))
                 t_form_EQs.start()
@@ -171,20 +213,35 @@ def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
             K_end = min(K,K_start+block_size)
             bsize = K_end-K_start
             t_o_slice.start()
-            nomega = omega[:,:,K_start : K_end]
+            if num_blocks > 1:
+                nomega = omega[:,:,K_start : K_end]
+            else:
+                nomega = omega
             t_o_slice.stop()
             x0 = ctf.random.random((bsize,r))
             b = ctf.tensor((bsize,r))
             t_RHS.start()
-            b.i("kr") << U.i("Ir")*V.i("Jr")* T[:,:,K_start : K_end].i("IJk")  # RHS; ATb
+            if num_blocks == 1:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T, [U, V, b], 2)
+                else:
+                    b.i("kr") << U.i("Ir")*V.i("Jr")* T.i("IJk")  # RHS; ATb
+            else:
+                if use_MTTKRP:
+                    ctf.MTTKRP(T[:,:,K_start : K_end], [U, V, b], 2)
+                else:
+                    b.i("kr") << U.i("Ir")*V.i("Jr")* T[:,:,K_start : K_end].i("IJk")  # RHS; ATb
             t_RHS.stop()
             if use_implicit:
                 Ax0 = ctf.tensor((bsize,r))
                 t_cg_TTTP.start()
-                Ax0.i("kr") << U.i("Ir")*V.i("Jr")*ctf.TTTP(nomega, [U,V,x0]).i("IJk")
+                if use_MTTKRP:
+                    ctf.MTTKRP(ctf.TTTP(nomega, [U,V,x0]), [U, V, Ax0], 2)
+                else:
+                    Ax0.i("kr") << U.i("Ir")*V.i("Jr")*ctf.TTTP(nomega, [U,V,x0]).i("IJk")
                 t_cg_TTTP.stop()
                 Ax0 += regParam*x0
-                W[K_start : K_end,:] = CG(implicit_ATA(U,V,nomega,"W"),b,x0,r,regParam,bsize,True)
+                W[K_start : K_end,:] = CG(implicit_ATA(U,V,nomega,"W",use_MTTKRP),b,x0,r,regParam,bsize,True)
             else:
                 A = ctf.tensor((bsize,r,r))
                 t_form_EQs.start()
@@ -194,7 +251,7 @@ def updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,string,use_implicit):
 
         return W
 
-def getALS_CG(T,U,V,W,regParam,omega,I,J,K,r,block_size,num_iter=100,err_thresh=.001,time_limit=600,use_implicit=True):
+def getALS_CG(T,U,V,W,regParam,omega,I,J,K,r,block_size,num_iter=100,err_thresh=.001,time_limit=600,use_implicit=True,use_MTTKRP=False):
     if use_implicit == True:
         t_ALS_CG = ctf.timer_epoch("als_CG_implicit")
         if ctf.comm().rank() == 0:
@@ -237,9 +294,9 @@ def getALS_CG(T,U,V,W,regParam,omega,I,J,K,r,block_size,num_iter=100,err_thresh=
         t_upd_cg = ctf.timer("ALS_upd_cg")
         t_upd_cg.start()
 
-        U = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"U",use_implicit)
-        V = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"V",use_implicit)
-        W = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"W",use_implicit)
+        U = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"U",use_implicit,use_MTTKRP)
+        V = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"V",use_implicit,use_MTTKRP)
+        W = updateFactor(T,U,V,W,regParam,omega,I,J,K,r,block_size,"W",use_implicit,use_MTTKRP)
 
         duration = time.time() - t_before_loop - t_obj_calc
         t_b_obj = time.time()
